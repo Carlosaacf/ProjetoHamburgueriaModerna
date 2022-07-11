41539aab182651118b5c876cfe1831f1cde68217
carlosaacf <carlosaacf@gmail.com>
2022-07-11 19:33:01 -0300

    primeiro commit

---
+++ Hamburguer.java
@@ -0,0 +1,21 @@
+package Hamburgueria;
+
+public class Hamburguer {
+
+
+   public String TipoHamburguer;
+
+    public String TipoQueijo;
+
+    public String AcrescentaVerdura;
+
+    public Hamburguer(String tipoHamburguer, String tipoQueijo, String tipoAcompanhamento) {
+        TipoHamburguer = tipoHamburguer;
+        TipoQueijo = tipoQueijo;
+        AcrescentaVerdura = AcrescentaVerdura;
+    }
+
+    public Hamburguer() {
+
+    }
+}
---
+++ PedidoHamburguer.java
@@ -0,0 +1,94 @@
+package Hamburgueria;
+
+import java.util.Scanner;
+
+public class PedidoHamburguer extends Hamburguer {
+
+    public PedidoHamburguer(String tipoHamburguer, String tipoQueijo, String AcrescentaVerdura) {
+        super(tipoHamburguer, tipoQueijo, AcrescentaVerdura);
+    }
+
+    public static void main(String args[]) {
+
+        Hamburguer lanche1 = new Hamburguer();
+
+
+        Scanner sc = new Scanner(System.in);
+        int opcao = 0;
+
+
+        while (opcao != 5) {
+            System.out.println("(1) Escolha o Tipo de Hamburguer: ");
+            System.out.println("(2) Escolher Queijo");
+            System.out.println("(3) Acrescentar Verdura");
+            System.out.println("(4) Imprimir Pedidos");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("1 - Carne");
+                    System.out.println("2 - Frango");
+                    System.out.println("3 - Soja");
+                    switch (opcao) {
+                        case 1:
+
+                            lanche1.TipoHamburguer = "Carne";
+                            break;
+                        case 2:
+
+                            lanche1.TipoHamburguer = "Frango";
+                            break;
+                        case 3:
+
+                            lanche1.TipoHamburguer = "Soja";
+                            break;
+
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("1 - Prato");
+                    System.out.println("2 - Provolone");
+                    System.out.println("3 - Cheddar");
+
+                    switch (opcao) {
+                        case 1:
+
+                            lanche1.TipoQueijo = "Prato";
+                            break;
+                        case 2:
+
+                            lanche1.TipoQueijo = "Provolone";
+                            break;
+                        case 3:
+
+                            lanche1.TipoQueijo = "Cheddar";
+                            break;
+                    }
+
+                case 3:
+                    System.out.println("1 - Acrescentar verdura");
+                    System.out.println("2 - Sem verdura");
+                    switch (opcao) {
+                        case 1:
+
+                            lanche1.AcrescentaVerdura = "Com Verdura";
+                            break;
+                        case 2:
+
+                            lanche1.AcrescentaVerdura = "Sem Verdura";
+                            break;
+                    }
+
+                case 4:
+                    System.out.println("====== Pedido =====");
+                    System.out.println("Seu pedido foi um Hamburguer de : " + lanche1.TipoHamburguer);
+                    System.out.println("Com queijo : " + lanche1.TipoQueijo + " E o complemento foi " + lanche1.AcrescentaVerdura);
+                    break;
+                default:
+
+                    System.out.println("Obrigado, volte sempre!");
+            }
+        }
+    }
+}
